4fbe7db012a33e394f69742bc7b07580aaa89c46
root <root@server.sebastian-christoph.de>
2019-05-28 14:19:25 +0200

    all new

--- Resources/Public/JavaScript/navigation.js
+++ Resources/Public/JavaScript/navigation.js
@@ -1,9 +1,6 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+});
+
+
